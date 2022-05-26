0e43d290881c66153a25882e35109710e927ba34
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2022-05-26 13:31:28 +0530

    Create NumPy program to find the real and imaginary parts of an array of complex numbers.py

---
+++ NUMPY/NumPy program to find the real and imaginary parts of an array of complex numbers.py
@@ -0,0 +1,11 @@
+import numpy as np
+x = np.sqrt([1+0j])
+y = np.sqrt([0+1j])
+print("Original array:x ",x)
+print("Original array:y ",y)
+print("Real part of the array:")
+print(x.real)
+print(y.real)
+print("Imaginary part of the array:")
+print(x.imag)
+print(y.imag)
