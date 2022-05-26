5773714a734019554f8e6b56b7e2c9c07e3d9328
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2022-05-26 13:30:15 +0530

    Create NumPy program to append values to the end of an array.py

---
+++ NUMPY/NumPy program to append values to the end of an array.py
@@ -0,0 +1,7 @@
+import numpy as np
+x = [10, 20, 30]
+print("Original array:")
+print(x)
+x = np.append(x, [[40, 50, 60], [70, 80, 90]])
+print("After append values to the end of the array:")
+print(x)
