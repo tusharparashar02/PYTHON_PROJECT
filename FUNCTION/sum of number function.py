6c4dc0decd28a71db9bbd7b0a1a7e3907dd5c6bd
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2022-05-26 13:22:01 +0530

    Create sum of number function.py

---
+++ FUNCTION/sum of number function.py
@@ -0,0 +1,12 @@
+# Write a Python function to sum all the numbers in a list.
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+    © 2022 GitHub, Inc.
+
+    Terms
